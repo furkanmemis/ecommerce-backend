40949f9083b697ff5fd8ab4cd117d0acb30bc3d3
furkanmemis <fmemis0160@gmail.com>
2026-01-03 16:41:24 +0300

    fixed route paths

--- stock/src/main/java/com/stock/controllers/CategoryController.java
+++ stock/src/main/java/com/stock/controllers/CategoryController.java
@@ -8,6 +8,7 @@ import com.stock.services.CategoryService;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
@@ -30,8 +31,8 @@ public class CategoryController {
         return this.categoryService.CreateCategory(category);
     }
 
-    @GetMapping("/get-category")
-    public Optional<Category> getMethodName(@RequestParam UUID id) {
+    @GetMapping("/get-category/{id}")
+    public Optional<Category> getMethodName(@PathVariable UUID id) {
         return this.categoryService.GetCategoryById(id);
     }
 
--- stock/src/main/java/com/stock/controllers/ProductController.java
+++ stock/src/main/java/com/stock/controllers/ProductController.java
@@ -8,6 +8,7 @@ import com.stock.services.ProductService;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
@@ -30,8 +31,8 @@ public class ProductController {
         return this.productService.CreateProduct(product);
     }
 
-    @GetMapping("/get-product")
-    public Optional<Product> getMethodName(@RequestParam UUID id) {
+    @GetMapping("/get-product/{id}")
+    public Optional<Product> getMethodName(@PathVariable UUID id) {
         return this.productService.GetProductById(id);
     }
 
